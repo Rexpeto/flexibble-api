f39ccda4313693969e6aad1f6fcdfdcfa46852e9
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-03 18:13:59 -0400

    Initial controller project

---
+++ src/controllers/Project.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+
+/*
+ * Search project by name
+ * @returns
+ * */
+export const getProject = (req: Request, res: Response) => {};
+
+/*
+ * All projects
+ * @returns
+ * */
+export const getProjects = (req: Request, res: Response) => {};
+
+/*
+ * Create project
+ * @returns
+ * */
+export const setProject = (req: Request, res: Response) => {};
+
+/*
+ * Update project
+ * @returns
+ * */
+export const updateProject = (req: Request, res: Response) => {};
+
+/*
+ * Delete project
+ * @returns
+ * */
+export const deleteProject = (req: Request, res: Response) => {};
