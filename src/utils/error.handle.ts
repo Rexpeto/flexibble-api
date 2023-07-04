927ee1f68f67bcddf9098f98b1e4caa4cbab38d2
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-04 13:07:55 -0400

    Show error in terminal

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,7 +1,8 @@
 import { Response } from "express";
 
-const handleHttp = (res: Response, error: string) => {
-    res.status(500).json({ error });
+const handleHttp = (res: Response, msg: string, error: any) => {
+    console.log(error);
+    res.status(500).json({ msg });
 };
 
 export default handleHttp;
