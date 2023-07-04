4d8b4659079d5a0143b3fc69c0ead5cc52bf596c
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-04 15:50:06 -0400

    Login user

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -1,7 +1,8 @@
 import UserInterface from "../interfaces/User.interface";
-import { AuthRegister } from "../interfaces/authUser.interface";
+import { AuthLogin, AuthRegister } from "../interfaces/authUser.interface";
 import User from "../models/User.model";
-import { encrypt } from "../utils/bcrypt.handle";
+import { encrypt, verified } from "../utils/bcrypt.handle";
+import { signToken } from "../utils/jwt.handle";
 
 export const registerNewUser = async ({
     name,
@@ -25,4 +26,33 @@ export const registerNewUser = async ({
     return newUser;
 };
 
-export const loginUser = async (email: string, password: string) => {};
+export const loginUser = async ({ email, password }: AuthLogin) => {
+    try {
+        const checkIs: UserInterface | null = await User.findOne({ email });
+
+        if (!checkIs) {
+            return { msg: "El usuario o la contraseña es incorrecta" };
+        }
+
+        const passwordHash = checkIs.password;
+        const isCorrect = await verified(password, passwordHash);
+
+        if (!isCorrect) {
+            return { msg: "El usuario o la contraseña es incorrecta" };
+        }
+
+        const { _id, name, email: emailUser, avatarUrl } = checkIs;
+        const jwt = signToken(_id, name, emailUser);
+
+        return {
+            _id,
+            name,
+            email: emailUser,
+            avatarUrl,
+            accessToken: jwt
+        };
+    } catch (e) {
+        console.log(e);
+        return { msg: "El usuario o la contraseña es incorrecta" };
+    }
+};
