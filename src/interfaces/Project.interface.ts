6f6388bc95864af2937e3226ee3b165174123a7f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-03 20:56:51 -0400

    Interface of project

---
+++ src/interfaces/Project.interface.ts
@@ -0,0 +1,13 @@
+import { ObjectId, SchemaDefinitionProperty } from "mongoose";
+
+interface ProjectInterface {
+    title: string;
+    description: string;
+    image: string;
+    liveSiteUrl?: string;
+    githubUrl?: string;
+    category: string;
+    createBy: SchemaDefinitionProperty<ObjectId>;
+}
+
+export default ProjectInterface;
