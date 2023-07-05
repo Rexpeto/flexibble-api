576d8c330c9e29db38bcb971c4ad0e1974ac1dd0
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-05 18:54:35 -0400

    Route newProject

--- src/routes/Project.route.ts
+++ src/routes/Project.route.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import sessionActive from "../middleware/session";
-import { getProjects } from "../controllers/Project.controller";
+import { getProjects, setProject } from "../controllers/Project.controller";
+import { uploadImgProject } from "../middleware/file";
 
 export const router = Router();
 
 router.get("/", getProjects);
+router.post(
+    "/newProject",
+    sessionActive,
+    uploadImgProject.single("image"),
+    setProject
+);
