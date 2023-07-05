4615f5701716dabf6ab9fa105a90c45ded0f92bf
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-05 13:12:30 -0400

    Use updateUser service

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from "express";
 import handleHttp from "../utils/error.handle";
-import { loginUser, registerNewUser } from "../services/User.service";
+import {
+    loginUser,
+    registerNewUser,
+    updateUser
+} from "../services/User.service";
+import ReqExt from "../interfaces/ReqExt.interface";
+import { JwtPayload } from "jsonwebtoken";
 
 /*
  * Login user
@@ -36,5 +42,26 @@ export const registerCtrl = async ({ body }: Request, res: Response) => {
         res.status(201).json(response);
     } catch (e) {
         handleHttp(res, "Oops! Ocurrio un error", e);
+    }
+};
+
+/*
+ * Update date of user
+ * @returns
+ * */
+export const updateUserCtrl = async (req: ReqExt, res: Response) => {
+    try {
+        const _id = req.user?.id;
+        const fileName = `${req.file?.filename}`;
+
+        const response: any = await updateUser(_id, req.body, fileName);
+
+        if (response?.msg) {
+            return res.status(403).json(response);
+        }
+
+        res.status(200).json(response);
+    } catch (e) {
+        handleHttp(res, "Oops!! Ocurrio un error", e);
     }
 };
