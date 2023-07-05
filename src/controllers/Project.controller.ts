b3175039e9dbeada42279a49d919dbe52e83ed47
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-05 18:53:49 -0400

    Use createProject service

--- src/controllers/Project.controller.ts
+++ src/controllers/Project.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import handleHttp from "../utils/error.handle";
-import { getAllProjects } from "../services/Project.service";
+import { createProject, getAllProjects } from "../services/Project.service";
+import ProjectInterface from "../interfaces/Project.interface";
+import ReqExt from "../interfaces/ReqExt.interface";
 
 /*
  * Search project by name
@@ -26,7 +28,40 @@ export const getProjects = (req: Request, res: Response) => {};
  * Create project
  * @returns
  * */
-export const setProject = (req: Request, res: Response) => {};
+export const setProject = async (req: ReqExt, res: Response) => {
+    const {
+        title,
+        description,
+        liveSiteUrl,
+        githubUrl,
+        category
+    }: ProjectInterface = req.body;
+    const image = `${req.file?.filename}`;
+    try {
+        const createBy = req?.user?.id;
+        const response: any = await createProject({
+            title,
+            description,
+            liveSiteUrl,
+            githubUrl,
+            category,
+            image,
+            createBy
+        });
+
+        if (response?.error) {
+            return res.status(500).json(response);
+        }
+
+        if (response.msg) {
+            return res.status(403).json(response);
+        }
+
+        res.status(200).json(response);
+    } catch (e) {
+        handleHttp(res, "Oops!! Ocurrio un error", e);
+    }
+};
 
 /*
  * Update project
