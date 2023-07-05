c6fd2dd9e6f7d9cfb829836423ef5b4f7006e9b3
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-05 14:37:12 -0400

    Controller getAllProjects

--- src/controllers/Project.controller.ts
+++ src/controllers/Project.controller.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from "express";
+import handleHttp from "../utils/error.handle";
+import { getAllProjects } from "../services/Project.service";
 
 /*
  * Search project by name
  * @returns
  * */
-export const getProject = (req: Request, res: Response) => {};
+export const getProject = async (req: Request, res: Response) => {
+    try {
+        const response = await getAllProjects();
+
+        res.status(200).json({ response });
+    } catch (e) {
+        handleHttp(res, "Oops!! Ocurrio un error", e);
+    }
+};
 
 /*
  * All projects
