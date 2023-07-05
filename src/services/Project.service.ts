dceddcc668f5717c975e4f25223d9d3d92259184
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-05 18:54:59 -0400

    Service create project

--- src/services/Project.service.ts
+++ src/services/Project.service.ts
@@ -7,3 +7,48 @@ export const getAllProjects = async () => {
 
     return projects;
 };
+
+export const createProject = async ({
+    title,
+    description,
+    liveSiteUrl,
+    githubUrl,
+    category,
+    image,
+    createBy
+}: any) => {
+    try {
+        if (!title) {
+            return { msg: "Debe colocar el titulo" };
+        }
+
+        if (!description) {
+            return { msg: "Debe colocar una descripción" };
+        }
+
+        if (image === "undefined") {
+            return { msg: "Debe agregar una imagen" };
+        }
+
+        if (!category) {
+            return { msg: "Debe agregar una categoría" };
+        }
+
+        const data = {
+            title,
+            description,
+            liveSiteUrl: liveSiteUrl ?? "",
+            githubUrl: githubUrl ?? "",
+            category: category ?? "",
+            image: `${process.env.IP_PUBLIC_SERVER}:${process.env.PORT}${process.env.ROUTE_PROJECT}${image}`,
+            createBy
+        };
+
+        const newProject = await Project.create(data);
+
+        return newProject;
+    } catch (e) {
+        console.log(e);
+        return { error: "Oops!! ocurrio un error" };
+    }
+};
