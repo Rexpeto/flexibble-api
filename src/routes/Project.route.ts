67c16b392a283b094cfa5447d0e02e8da7a41179
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-06 00:41:17 -0400

    Route of search project by id

--- src/routes/Project.route.ts
+++ src/routes/Project.route.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import sessionActive from "../middleware/session";
-import { getProjects, setProject } from "../controllers/Project.controller";
+import {
+    getProject,
+    getProjects,
+    setProject
+} from "../controllers/Project.controller";
 import { uploadImgProject } from "../middleware/file";
 
 export const router = Router();
 
 router.get("/", getProjects);
+router.post("/:id", sessionActive, getProject);
 router.post(
     "/newProject",
     sessionActive,
