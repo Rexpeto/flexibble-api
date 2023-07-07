c48934f8ba3c3b4fd5bc6da8ed19edffeaf361ed
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-07 14:55:35 -0400

    Delete project

--- src/routes/Project.route.ts
+++ src/routes/Project.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import sessionActive from "../middleware/session";
 import {
+    deleteProject,
     getProject,
     getProjects,
     setProject,
@@ -11,16 +12,21 @@ import { uploadImgProject } from "../middleware/file";
 export const router = Router();
 
 router.get("/", getProjects);
+
 router.post("/search/:id", sessionActive, getProject);
+
 router.post(
     "/newProject",
     sessionActive,
     uploadImgProject.single("image"),
     setProject
 );
+
 router.post(
     "/update/:id",
     sessionActive,
     uploadImgProject.single("image"),
     updateProject
 );
+
+router.post("/delete/:id", sessionActive, deleteProject);
