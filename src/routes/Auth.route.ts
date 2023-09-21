a3ff9243a5ed8c55e361488e6d35772932112f46
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-21 15:00:19 -0400

    login route doc

--- src/routes/Auth.route.ts
+++ src/routes/Auth.route.ts
@@ -3,5 +3,61 @@ import { loginCtrl, registerCtrl } from "../controllers/User.controller";
 
 export const router = Router();
 
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login user.
+ *     description: Login of user.
+ *     tags: [Auth]
+ *     requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 email:
+ *                  type: string
+ *                  description: The user's email.
+ *                  example: email@example.com
+ *                 password:
+ *                  type: string
+ *                  description: The user's password.
+ *                  example: 123456
+ *     responses:
+ *       200:
+ *         description: User logged.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                  _id:
+ *                      type: string
+ *                      description: The user ID.
+ *                      example: 5e1b7a0d3f9b2e1b7a0d3f9b
+ *                  name:
+ *                      type: string
+ *                      description: The user's name.
+ *                      example: Leanne Graham
+ *                  email:
+ *                      type: string
+ *                      description: The user's email.
+ *                      example: email@example.com
+ *                  avatarUrl:
+ *                      type: string
+ *                      description: The user's avatar.
+ *                      example: https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50
+ *                  rol:
+ *                      type: string
+ *                      description: The user's role.
+ *                      example: admin
+ *                  accessToken:
+ *                      type: string
+ *                      description: The user's token.
+ *                      example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjY3MjQwMzYyLCJleHAiOjE2NjcyNDA5NjJ9.
+ */
 router.post("/login", loginCtrl);
+
 router.post("/register", registerCtrl);
