b5b3dbd24ca664dbea61674babcfa80434e4b62d
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-22 12:45:17 -0400

    register route doc

--- src/routes/Auth.route.ts
+++ src/routes/Auth.route.ts
@@ -72,4 +72,82 @@ export const router = Router();
  *           */
 router.post("/login", loginCtrl);
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register user.
+ *     description: Register of user.
+ *     tags: [Auth]
+ *     requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                  type: string
+ *                  description: The user's name.
+ *                  example: Leanne Graham
+ *                 email:
+ *                  type: string
+ *                  description: The user's email.
+ *                  example: email@example.com
+ *                 password:
+ *                  type: string
+ *                  description: The user's password.
+ *                  example: 123456
+ *     responses:
+ *       401:
+ *         description: Reigster error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                     type: string
+ *                     description: The error message.
+ *                     example: El usuario ya existe
+ *       200:
+ *         description: Register success.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                  _id:
+ *                      type: string
+ *                      description: The user ID.
+ *                      example: 5e1b7a0d3f9b2e1b7a0d3f9b
+ *                  name:
+ *                      type: string
+ *                      description: The user's name.
+ *                      example: Leanne Graham
+ *                  email:
+ *                      type: string
+ *                      description: The user's email.
+ *                      example: email@example.com
+ *                  avatarUrl:
+ *                      type: string
+ *                      description: The user's avatar.
+ *                      example: https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50
+ *                  rol:
+ *                      type: string
+ *                      description: The user's role.
+ *                      example: admin
+ *                  accessToken:
+ *                      type: string
+ *                      description: The user's token.
+ *                      example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjY3MjQwMzYyLCJleHAiOjE2NjcyNDA5NjJ9.
+ *                  projects:
+ *                      type: array
+ *                      items:
+ *                          type: string
+ *                          exmaple: 5e1b7a0d3f9b2e1b7a0d3f9b
+ *                          description: The user's projects
+ *
+ *           */
+
 router.post("/register", registerCtrl);
