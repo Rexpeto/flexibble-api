51fbf8a0c118c273f0dd60cad5c61782615caeb5
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-23 16:47:52 -0400

    get all projects doc

--- src/routes/Project.route.ts
+++ src/routes/Project.route.ts
@@ -11,6 +11,25 @@ import { uploadImgProject } from "@/middleware/file";
 
 export const router = Router();
 
+/**
+ * @swagger
+ * /project:
+ *   get:
+ *     summary: Get all projects.
+ *     description: Shows all registered projects
+ *     tags: [Project]
+ *     responses:
+ *        200:
+ *         description: Show all projects of the user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                   $ref: "#/components/schemas/Project"
+ *
+ *
+ *           */
 router.get("/", getProjects);
 
 router.post("/search/:id", sessionActive, getProject);
